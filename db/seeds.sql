e86e7b47d3bbca2ed317c9e3c01be75e26a89938
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-11-03 02:16:00 -0500

    Created seeds pseudo-code

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+-- INSERT INTO department
+
+-- INSERT INTO role
+
+-- INSERT INTO employee
+
+-- For viewing data:
+-- Viewing all employees
+
+-- Viewing all employees by department
+
+-- Viewing all employees by manager
+
+-- Viewing departments
+
+-- Viewing department budgets
+
+-- For updating queries:
+-- Setting employee role
+
+-- Setting employee managers
+
+-- Queries to add new employees
+
+-- Queries to add new departments
