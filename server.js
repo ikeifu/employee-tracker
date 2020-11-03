2ad5a6e91c02324ba813f185706e550470a085f2
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-11-03 08:55:11 -0500

    Pseudo-coded outline for server

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+
+// Globals
+
+// Connection to MySQL
+
+// Agnostic functions to search IDs
+
+// Figlet Word art / top banner
+
+// Populate options list
+
+// Get Role IDS and titles
+
+// Get employee IDs and names
+
+// Get Department IDs and names
+
+// Inquirer prompt
+
+// Assign constructor functions to match inquirer options
+
+// Call functions
