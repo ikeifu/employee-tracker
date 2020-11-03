4a53c95e35aabf67d00bd45aaf23e10059306122
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-11-03 02:22:02 -0500

    Insert into role

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,6 +10,22 @@ USE employee_DB;
 SELECT * FROM department;
 
 -- INSERT INTO role
+USE employee_DB;
+INSERT INTO role (title, salary, department_id) VALUES ('Senior A&R', '82300', '1');
+INSERT INTO role (title, salary, department_id) VALUES ('Junior A&R', '45700', '1');
+INSERT INTO role (title, salary, department_id) VALUES ('Marketing Director', '109400', '2');
+INSERT INTO role (title, salary, department_id) VALUES ('Promtional Agent', '65050', '2');
+INSERT INTO role (title, salary, department_id) VALUES ('Distributor Liaison', '71000', '3');
+INSERT INTO role (title, salary, department_id) VALUES ('Financial Officer', '110000', '4');
+INSERT INTO role (title, salary, department_id) VALUES ('In-House Legal Council', '137050', '5');
+
+USE employee_DB;
+SELECT * FROM role;
+USE employee_DB;
+SELECT title
+    FROM role
+    -- ORDER BY department_id in ascending order, same with title
+    ORDER BY department_id ASC, title ASC;
 
 -- INSERT INTO employee
 
